f82219a782191db124b0cb4bd4e1ce6a835827eb
矢野大樹 <daikitech0123@gmail.com>
2021-07-08 21:30:19 +0900

    Active recodeで画像がアップロードできるようにする

--- app/controllers/api/v1/admin/sites_controller.rb
+++ app/controllers/api/v1/admin/sites_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::Admin::SitesController < ApplicationController
   # 管理人のみアクセス可能
-  before_action :authenticate_admin!
+  before_action :authenticate_api_v1_admin!
   def index
     @sites = Site.all
+    render json: @sites,methods: [:image_url]
   end
 
   def create
     @site = Site.new(site_params)
-    @site.admin_id = current_admin.id
+    @site.admin_id = current_api_v1_admin.id
     if @site.save
-      render json: @site, status: :ok
+      render json: @site, methods: [:image_url], status: :ok
     else
       render json: @site.errors, status: :unprocessable_entity
     end
@@ -27,6 +28,6 @@ class Api::V1::Admin::SitesController < ApplicationController
   private
 
   def site_params
-    params.permit(:name, :description, :url, :price,:admin_id)
+    params.permit(:name, :description, :url, :price, :image)
   end
 end
